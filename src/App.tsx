2ce713f84fde30fda545277abea8048fe7f7d4b1
royeradames <royeraadames@gmail.com>
2021-03-25 15:28:21 -0400

    📦add mobile layout, complete mobile header style

--- src/App.tsx
+++ src/App.tsx
@@ -19,80 +19,84 @@ function App() {
         </p>
       </header>
 
-      <section className="rate reviews-rate">
-        <div className="rate_starts">
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
+      <section className="rate">
+        <div className="rate_reviews">
+          <div className="rate_starts">
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+          </div>
+          <p className="rate__message">Rated 5 Stars in Reviews</p>
         </div>
-        <p className="rate__message">Rated 5 Stars in Reviews</p>
-      </section>
-      <section className="report-guru-rate rate">
-        <div className="rate_starts">
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
+        <div className="rate_report_gurux">
+          <div className="rate_starts">
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+          </div>
+          <p className="rate__message">Rated 5 Stars in Report Guru</p>
         </div>
-        <p className="rate__message">Rated 5 Stars in Report Guru</p>
-      </section>
-      <section className="rate best-tect-rate">
-        <div className="rate_starts">
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
-          <img src={ratingStart} alt="A rating start" />
+        <div className="rate_best_tect">
+          <div className="rate_starts">
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+            <img src={ratingStart} alt="A rating start" />
+          </div>
+          <p className="rate__message">Rated 5 Stars in BestTech</p>
         </div>
-        <p className="rate__message">Rated 5 Stars in BestTech</p>
       </section>
 
-      <section className="testimonial testimonial__colton-smith">
-        <img
-          src={colton}
-          alt="portrait of Colton Smith"
-          className="testimonial__img"
-        />
-        <h2 className="testimonial__name">Colton Smith</h2>
-        <p className="testimonial__verify-buyer">Verified Buyer</p>
-        <p className="testimonial__message">
-          "We needed the same printed design as the one we had ordered a week
-          prior. Not only did they find the original order, but we also received
-          it in time. Excellent!"
-        </p>
-      </section>
+      <section className="testimonials">
+        <div className="testimonial__colton_smith">
+          <img
+            src={colton}
+            alt="portrait of Colton Smith"
+            className="testimonial__img"
+          />
+          <h2 className="testimonial__name">Colton Smith</h2>
+          <p className="testimonial__verify-buyer">Verified Buyer</p>
+          <p className="testimonial__message">
+            "We needed the same printed design as the one we had ordered a week
+            prior. Not only did they find the original order, but we also
+            received it in time. Excellent!"
+          </p>
+        </div>
 
-      <section className="testimonial testimonial__irene-roberts">
-        <img
-          src={irene}
-          alt="portrait of Irene Roberts"
-          className="testimonial__img"
-        />
-        <h2 className="testimonial__name">Irene Roberts</h2>
-        <p className="testimonial__verify-buyer">Verified Buyer</p>
-        <p className="testimonial__message">
-          "Customer service is always excellent and very quick turn around.
-          Completely delighted with the simplicity of the purchase and the speed
-          of delivery."
-        </p>
-      </section>
+        <div className="testimonial__irene_roberts">
+          <img
+            src={irene}
+            alt="portrait of Irene Roberts"
+            className="testimonial__img"
+          />
+          <h2 className="testimonial__name">Irene Roberts</h2>
+          <p className="testimonial__verify-buyer">Verified Buyer</p>
+          <p className="testimonial__message">
+            "Customer service is always excellent and very quick turn around.
+            Completely delighted with the simplicity of the purchase and the
+            speed of delivery."
+          </p>
+        </div>
 
-      <section className="testimonial testimonial__anne-wallance">
-        <img
-          src={anne}
-          alt="portrait of Anne Wallace"
-          className="testimonial__img"
-        />
-        <h2 className="testimonial__name">Anne Wallace</h2>
-        <p className="testimonial__verify-buyer">Verified Buyer</p>
-        <p className="testimonial__message">
-          "Put an order with this company and can only praise them for the very
-          high standard. Will definitely use them again and recommend them to
-          everyone!"
-        </p>
+        <div className="testimonial__anne_wallance">
+          <img
+            src={anne}
+            alt="portrait of Anne Wallace"
+            className="testimonial__img"
+          />
+          <h2 className="testimonial__name">Anne Wallace</h2>
+          <p className="testimonial__verify-buyer">Verified Buyer</p>
+          <p className="testimonial__message">
+            "Put an order with this company and can only praise them for the
+            very high standard. Will definitely use them again and recommend
+            them to everyone!"
+          </p>
+        </div>
       </section>
     </>
   );
